a6d0b45198f468767e7fdeb31c10b2946db7ec19
lifeforest <nij2@TDD00651134.TDBFG.com>
2015-06-19 14:49:07 -0400

    Added my own code for cachematrix.R
    The code adds an extra piece to remove the inverse. It's easy to test
    and see how the other functions work.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,54 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## makeCacheMatrix creates a special vector that can:
+## 1. set the value of the matrix
+## 2. get the value of the matrix
+## 3. set the value of the inverse  
+## 4. get the value of the inverse  
+## 5. remove the value of the inverse (not required in the assignment)
 
 makeCacheMatrix <- function(x = matrix()) {
+  
+  i <- NULL
+  set <- function(y){
+    x <<- y
+    i <<- NULL
+  }
+  get <- function () x
+  setInverse <- function(solve) i <<- solve
+  getInverse <- function() i
+  
+  releaseInverse <- function() {
+    i <<- NULL
+  } 
+  
+  list(set = set, get = get, 
+       setInverse = setInverse, 
+       getInverse = getInverse,
+       releaseInverse = releaseInverse
+  )
+  
 
 }
 
 
-## Write a short comment describing this function
+## This function takes a vector created by function makeCacheMatrix.
+## It then check if the inverse has already been calculated. If so then 
+## first print the sentence "getting cached data", then return the inverse.
+## Otherwise, calcualte the inverse and return it.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  
+  i <- x$getInverse()
+  if(!is.null(i)) {
+    message("getting cached data")
+    return(i)
+  }
+  
+  data <- x$get()
+  i <- solve(data, ...)
+  x$setInverse(i)
+  i 
 }
